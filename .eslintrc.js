0029d54bd9dd6293b1f3b10a9c2890eb1660d659
Rafael sanchez <eduardoxlau@hotmail.com>
2020-09-23 17:20:10 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    quotes: ["error", "double"],
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ],
+  },
+};
