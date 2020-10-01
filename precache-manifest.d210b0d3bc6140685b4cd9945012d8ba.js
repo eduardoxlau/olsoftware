a021e80b96d76a047f0c52dd6324deef335d00d3
Rafael sanchez <eduardoxlau@hotmail.com>
2020-09-30 21:26:05 -0500

    Updates

---
+++ precache-manifest.d210b0d3bc6140685b4cd9945012d8ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3c5afa56027a5db74c78dd82a2554c",
+    "url": "/olsoftware/index.html"
+  },
+  {
+    "revision": "f006310cd1fa2c2b7327",
+    "url": "/olsoftware/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0a4c2d4c173a0fcee305",
+    "url": "/olsoftware/static/css/main.d4b803a7.chunk.css"
+  },
+  {
+    "revision": "f006310cd1fa2c2b7327",
+    "url": "/olsoftware/static/js/2.baffe464.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/olsoftware/static/js/2.baffe464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4c2d4c173a0fcee305",
+    "url": "/olsoftware/static/js/main.abb26c8a.chunk.js"
+  },
+  {
+    "revision": "bf1a569a15a1909c42b0",
+    "url": "/olsoftware/static/js/runtime-main.a23b2467.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/olsoftware/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "d3af203c87d76274f6f69e70d45ddfbf",
+    "url": "/olsoftware/static/media/loginBackGround.d3af203c.jpg"
+  }
+]);
